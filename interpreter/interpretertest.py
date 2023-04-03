ec52cdbdf113574f58a13f635170f9b19e09fe53
Pavan Boyapati <pavan.boyapati@scdhhs.gov>
2023-04-03 18:05:53 -0400

    04/03

---
+++ interpreter/interpretertest.py
@@ -0,0 +1,17 @@
+#!/Users/TMP25257/AppData/Local/Programs/Python/Python310 python
+import sys
+inputText = input("Enter Text to exit to quit program or quit to quit\n")
+
+if (inputText == 'exit'):
+    print('^Z\n')
+    breakpoint()
+    print('YES')
+    print('next line')
+elif (inputText == 'quit'):
+    quit()
+    print(YES)
+else:
+    exit()
+    print(YES)
+
+print(sys.argv[0])
