78851f6db33737f455a30d71945ee7f83cfd6719
Pavan Boyapati <pavan.boyapati@scdhhs.gov>
2024-09-26 21:54:33 -0400

    daily learning 09_26

--- ch_04_LoopsandconditionalExecution/loops.py
+++ ch_04_LoopsandconditionalExecution/loops.py
@@ -1,12 +1,68 @@
-largest_number = -999999999
-number = int(input("Enter Largest number : "))
-if number == -1:
-    print(largest_number)
-    exit()
-if number > largest_number:
-    largest_number = number
+def for_loop():
+    """
+    ==============================
+    Explanation
+    ==============================
+    for loop is used to iterate over a sequence (list, tuple, string, etc.)
+    ==============================
+    Syntax
+    ==============================
+    for <variable> in <sequence>:
+            <statement>
+            <statement>
+    
+            ...
+    ==============================
+    Example
+    ==============================
+    for i in [1, 2, 3, 4, 5]:
+        print(i)
+    ==============================
+    Output
+    ==============================
+    1
+    2
+    3
+    4
+    5    
+    """
+    print(for_loop.__doc__)
 
-val1 = 10
-val2 = 5
-print(val1 | val2)
-# Go to line 02
+def while_loop():
+    """
+    ==============================
+    Explanation
+    ==============================
+    while loop is used to iterate over a sequence until a condition is met
+    ==============================
+    Syntax
+    ==============================
+    while <condition>:
+            <statement>
+            <statement>
+            ...
+    ==============================
+    Example
+    ==============================
+    i = 0
+    while i < 5:
+        print(i)
+        i += 1
+    ==============================
+    Output
+    ==============================
+    0
+    1
+    2
+    3
+    4
+    """
+    print(while_loop.__doc__)
+    
+
+if __name__ == "__main__":
+    loop_type = input("Python loops are For and while. Which loop you want to learn? Please type `for` to learn `for` and `while` to learn `while` \n")
+    if loop_type == "for":
+        for_loop()
+    elif loop_type == "while":
+        while_loop()        
