119d310ecf8abd0832bc5ef771740c847d259106
Pavan Boyapati <pavan.boyapati@scdhhs.gov>
2025-01-23 22:02:43 -0500

    Daily checkin 01/23

--- ch_08_Object_oriented_programming/stack.py
+++ ch_08_Object_oriented_programming/stack.py
@@ -1,6 +1,39 @@
-class stack:
+class Stack:
     def __init__(self):
-        print("stack Object is created")
+        self.__stackList = []
+
+    def push(self, element):
+        self.__stackList.append(element)
+
+    def pop(self):
+        val = self.__stackList[-1]
+        del self.__stackList[-1]
+        return val
+
+class AddingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__sum = 0
+
+    def push(self, element):
+        self.__sum += element
+        Stack.push(self, element)
+
+    def pop(self):
+        val = Stack.pop(self)
+        self.__sum -= val
+        return val
+    
+    def getSum(self):
+        return self.__sum
+        
 
 
-stack1 = stack()
+stackObject = AddingStack()
+
+for i in range(5):
+    stackObject.push(i)
+print(stackObject.getSum())
+
+for i in range(5):
+    print(stackObject.pop())
