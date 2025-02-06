bb9c2ec6d7fa252d4c76018ffd93e1dccf733cf8
Pavan Boyapati <pavan.boyapati@scdhhs.gov>
2025-02-05 21:11:56 -0500

    Daily check-in 02/05/25

---
+++ ch_08_Object_oriented_programming/Generators.py
@@ -0,0 +1,84 @@
+class Fib:
+	def __init__(self, nn):
+		self.__n = nn
+		self.__i = 0
+		self.__p1 = self.__p2 = 1
+
+	def __iter__(self):
+		print("Fib iter")
+		return self
+
+	def __next__(self):
+		self.__i += 1
+		if self.__i > self.__n:
+			raise StopIteration
+		if self.__i in [1, 2]:
+			return 1
+		ret = self.__p1 + self.__p2
+		self.__p1, self.__p2 = self.__p2, ret
+		return ret
+
+class Class:
+	def __init__(self, n):
+		self.__iter = Fib(n)
+
+	def __iter__(self):
+		print("Class iter")
+		return self.__iter;
+
+
+def fun(n):
+    print('Function entered')
+    for i in range(n):
+        yield i
+    print('Function exited')
+    
+for i in fun(5):
+    pass #print(i)
+
+def powersOf2(n):
+    pow = 1
+    for i in range(n):
+        yield pow
+        pow *= 2
+
+for v in powersOf2(8):
+    pass #print(v)
+
+t = [x for x in powersOf2(5)]
+#print(t)
+
+t = list(powersOf2(3))
+#print(t)
+
+def powersOf2(n):
+    pow = 1
+    for i in range(n):
+        yield pow
+        pow *= 2
+
+for i in range(20):
+    if i in powersOf2(4):
+        pass #print(i)
+
+
+
+object = Class(8)
+
+for i in object:
+	pass #print(i)
+
+
+#Fibonacci number generator
+def fib(n):
+    p1 = p2 = 1
+    for i in range(n):
+        if i in [1, 2]:
+            yield 1
+        else:
+            ret = p1 + p2
+            p1, p2 = p2, ret
+            yield ret
+
+for i in fib(500):
+    print(i)
